dc0b49b06f22f3d33e30f4e8b482c34d703b0509
accwill <songyaqigg@gmail.com>
2022-11-05 19:55:07 +0800

    feat: 383. 赎金信优化

--- 383. 赎金信.js
+++ 383. 赎金信.js
@@ -34,13 +34,11 @@
  * @param {string} magazine
  * @return {boolean}
  */
- var canConstruct = function(ransomNote, magazine) {
+var canConstruct = function(ransomNote, magazine) {
   /**
    * 统计ransomNote中的每个字符的个数，判断magazin中相应地字符个数
    * 如果ransomNote中有的字符，magazine中没有则返回false，或者ransomNote中的字符个数大于magazine中的字符个数返回false
    * 否则返回true
-   * 
-   * 
    */
   
   const ransomNoteObj = {}
@@ -60,7 +58,6 @@
     }
     magazineObj[magazine[i]]++
   }
-  console.log(ransomNoteObj, magazineObj)
   for (const key in ransomNoteObj) {
     if (!magazineObj[key]) {
       return false
@@ -71,4 +68,31 @@
   }
   return true
 };
-console.log('canConstruct', canConstruct('aa', 'aab'))
+var canConstruct1 = function(ransomNote, magazine) {
+  /**
+  * 统计magazine中的每个字符的出现的次数存在字典中，键为字符，键值为字符出现的次数
+  * 循环ransomNote，对字典中的键值做减法，如果键值小于0或者不存在则返回false，否则返回true
+  */
+
+  const dic = {}
+
+  for (let i = 0; i < magazine.length; i++) {
+    if (!dic[magazine[i]]) {
+      dic[magazine[i]] = 1
+      continue;
+    }
+    dic[magazine[i]]++
+  }
+  for (let i = 0; i < ransomNote.length; i++) {
+    // 书写顺序一定不能乱
+    if (dic[ransomNote[i]] === undefined) {
+      return false
+    }
+    dic[ransomNote[i]]--
+    if (dic[ransomNote[i]] < 0) {
+      return false
+    }
+  }
+  return true
+};
+console.log('canConstruct1', canConstruct1('aa', 'aab'))
