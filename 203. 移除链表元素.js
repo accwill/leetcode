06979da2cab31c0c504a8bf2b33a12ed8858ddd6
accwill <songyaqigg@gmail.com>
2022-11-06 18:08:05 +0800

    add: 203. 移除链表元素

--- 203. 移除链表元素.js
+++ 203. 移除链表元素.js
@@ -12,5 +12,40 @@
  */
  var removeElements = function(head, val) {
    
+  const h = new ListNode()
+  h.next = head
+  let p = h
+  while(p) {
+    if (p.val === val) {
+      // 删除节点
+      if (prev === null) {
+        head = head.next
+      } else {
+        prev.next = p.next
+        p = prev
+      }
+    }
+    prev = p
+    p = p.next
+  }
+
+  return h.next
+
+};
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+ var removeElements = function(head, val) {
+   
+
+  if (head === null) {
+    return null
+  }
+
+  head.next = removeElements(head.next, val)
+
+  return head.val === val ? head.next : head
 
 };
